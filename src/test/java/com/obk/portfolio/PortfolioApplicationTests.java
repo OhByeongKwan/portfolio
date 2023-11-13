b1ecff39f3f36fdbb59b62b8a43f1de88e56636f
오병관 <obk9184@naver.com>
2023-11-13 21:07:41 +0900

    start!

---
+++ src/test/java/com/obk/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obk.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
